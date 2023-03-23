bf98dc00954ca78a562f7f364923d4ae594f9943
KGTH <kgthcruz94@gmail.com>
2023-03-23 16:51:29 +0000

    se comienza login

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, jwt_required , get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,26 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])   #registro de usuario
+def register():
+    data = request.json
+    try:
+        user = User(name=data['name'], email=data['email'], password=data['password'])
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e: 
+        print(e)
+        return jsonify({"msg": "Error"}),400
+    
+    return jsonify({"msg": "User created"}),200
+
+@api.route('/login', methods=['POST'])  #login de usuario    
+def login():
+    data = request.json 
+    user = User.query.filter_by(email=data['email'], password=data['password']).first()
+    if user:
+        access_token = create_access_token(identity=user.id)   
+        return jsonify({"token": access_token}), 200
+    
+    return jsonify({"msg": "Wrong user/password"}), 400
